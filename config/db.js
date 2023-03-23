8efc52fab95f376bc78b3cc8bfc3da642fde02df
Rohan Maher <rohanmaher97@gmail.com>
2023-03-23 19:17:58 +0530

    Added

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const { checkout } = require("../app")
 module.exports ={
     'secret' : "10xauthsecret",
     // 'db':"mongodb://localhost:27017/node-auth"
-    'db':"mongodb+srv://RohanMaher:RohanMahadev<password>@cluster0.bznr4qj.mongodb.net/?retryWrites=true&w=majority"
+   'db': process.env.DB_url
+
 }
-
-
